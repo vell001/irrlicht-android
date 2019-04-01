df09e6c2bc159b0ddcb6072a7cca5d147ee742e0
vellhe <vellhe@tencent.com>
2019-04-01 20:06:53 +0800

    * 调通Irrlicht android demo

---
+++ app/src/main/java/vell/bibi/irrlicht_demo/MainActivity.java
@@ -0,0 +1,10 @@
+package vell.bibi.irrlicht_demo;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+public class MainActivity extends NativeActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
